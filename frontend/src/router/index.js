c3fe0078b9148fe2ae22740e3f35d28f5d0fd6a9
Hogyu01 <hogyu0507@gmail.com>
2026-07-30 16:26:55 +0900

    feat: 마이페이지 구현 #12

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -27,6 +27,11 @@ const routes = [
     component: () => import('@/views/benefit/BenefitMatchView.vue'),
     props: { initialTab: 'kb' },
   }, // 12-2 KB
+  {
+    path: '/mypage',
+    name: 'mypage',
+    component: () => import('@/views/mypage/MyPageView.vue'),
+  },
 ];
 
 const router = createRouter({
