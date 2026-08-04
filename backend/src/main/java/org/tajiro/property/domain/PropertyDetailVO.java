8a35b2fb463291ab7728fb5e8db6a51d7a2c4031
김재현 <0908jhk@naver.com>
2026-08-04 16:45:01 +0900

    fix: 매물 상세 내역 조회 API 데이터 수정 #62

--- backend/src/main/java/org/tajiro/property/domain/PropertyDetailVO.java
+++ backend/src/main/java/org/tajiro/property/domain/PropertyDetailVO.java
@@ -33,8 +33,8 @@ public class PropertyDetailVO {
     private Boolean parkAvailability;
     private Boolean transactionStatus;
     private String propertyDescription;
-    private LocalDateTime availableDate;
     private LocalDateTime moveInDate;
+    private LocalDateTime availableDate;
     private LocalDateTime updateDate;
     private Boolean discussionStatus;
     private String dong;
