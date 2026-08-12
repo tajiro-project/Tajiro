aef9df0587eee6b29c1d333d6600f1a50dc0bbe7
Minju Kang <brio0106@gmail.com>
2026-08-12 13:39:48 +0900

    fix: 월세 가격 점수에서 관리비 제외 BE #261

--- backend/src/main/java/org/tajiro/property/service/PropertyScoreServiceImpl.java
+++ backend/src/main/java/org/tajiro/property/service/PropertyScoreServiceImpl.java
@@ -208,14 +208,9 @@ public class PropertyScoreServiceImpl implements PropertyScoreService {
                         property.getDeposit(),
                         preference.getMinDeposit(),
                         preference.getMaxDeposit());
-                Integer monthlyRent = property.getMonthlyRent();
-                Integer monthlyCost = monthlyRent == null
-                        ? null
-                        : monthlyRent + (property.getMaintenanceFee() == null
-                                ? 0
-                                : property.getMaintenanceFee());
+
                 double rentScore = lowerIsBetter(
-                        monthlyCost,
+                        property.getMonthlyRent(),
                         preference.getMinMonthlyRent(),
                         preference.getMaxMonthlyRent());
                 return depositScore * MONTHLY_DEPOSIT_WEIGHT
