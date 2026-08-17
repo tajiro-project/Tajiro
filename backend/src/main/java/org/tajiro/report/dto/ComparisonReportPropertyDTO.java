1f39d3ed50a85a711fa33f769ae6e8fac1937d4c
Minju Kang <brio0106@gmail.com>
2026-08-17 22:18:16 +0900

    refactor: 매물 수정 시각을 DATETIME으로 변경 #349

--- backend/src/main/java/org/tajiro/report/dto/ComparisonReportPropertyDTO.java
+++ backend/src/main/java/org/tajiro/report/dto/ComparisonReportPropertyDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -22,5 +22,5 @@ public class ComparisonReportPropertyDTO {
     private Integer maintenanceFee;
     private BigDecimal areaM2;
     private String floorInfo;
-    private LocalDate updatedDate;
+    private LocalDateTime updatedDate;
 }
